92594c159e2153ad6b81bf6e9e983d0e3a9e5592
ku00 <skentaro36@gmail.com>
2020-04-17 18:42:31 +0900

    Interpolスキーマからリクエストパラメータを抽出できるようにした

--- lib/interpol/schema_parser.rb
+++ lib/interpol/schema_parser.rb
@@ -8,6 +8,19 @@ module Interpol
       @schema = YAML.load_file path
     end
 
+    def request_params_in_path
+      schema.dig("definitions", 0, "path_params")
+    end
+
+    def request_params_in_query
+      schema.dig("definitions", 0, "query_params")
+    end
+
+    # For OpenAPI v2
+    def request_params_in_body
+      schema.dig("definitions", 0, "query_params")
+    end
+
     def response_params
       schema.dig("definitions", 1, "schema")
     end
