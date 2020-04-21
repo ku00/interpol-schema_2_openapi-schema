d7d3cce71e17d64cc52a565dc621ac7679ab4547
ku00 <skentaro36@gmail.com>
2020-04-21 11:13:46 +0900

    OpenAPIスキーマの一部分（Path Object）しか生成できないので、それに合った名前空間をつける
    インスタンス変数やメソッド名もより適切そうな名称に変更した

--- lib/interpol_schema_2_openapi_schema.rb
+++ lib/interpol_schema_2_openapi_schema.rb
@@ -1,6 +1,6 @@
 require "interpol_schema_2_openapi_schema/version"
 require "interpol_schema/parser"
-require "openapi_schema/builder"
+require "openapi_schema/path_object/builder"
 
 module InterpolSchema2OpenapiSchema
   class Error < StandardError; end
@@ -8,6 +8,6 @@ module InterpolSchema2OpenapiSchema
   def self.execute(path)
     # debug
     schema = InterpolSchema::Parser.new(path)
-    OpenapiSchema::Builder.new(schema)
+    OpenapiSchema::PathObject::Builder.new(schema)
   end
 end
---
+++ lib/openapi_schema/path_object/builder.rb
@@ -0,0 +1,82 @@
+module OpenapiSchema
+  module PathObject
+    class Builder
+      attr_reader :interpol_schema
+
+      def initialize(interpol_schema)
+        @interpol_schema = interpol_schema
+      end
+
+      def base_template
+        {
+          "#{interpol_schema.http_method.downcase}" => {
+            "summary" => "summary",
+            "description" => "description",
+            "parameters" => request_params,
+            "responses" => {
+              "#{interpol_schema.response_code}" => {
+                "description" => "response description",
+                "schema" => interpol_schema.response_params
+              }
+            }
+          }
+        }
+      end
+
+      def request_params
+        [
+          request_params_in_path_template,
+          request_params_in_query_template,
+        ].flatten.compact
+      end
+
+      def request_params_in_path_template
+        return if empty_param?(interpol_schema.request_params_in_path)
+
+        interpol_schema.request_params_in_path["properties"].map do |name, detail|
+          {
+            "in" => "path",
+            "name" => name,
+            "type" => detail["type"],
+            "required" => true,
+          }.merge(
+            enum_params_template(detail)
+          )
+        end
+      end
+
+      def request_params_in_query_template
+        return if empty_param?(interpol_schema.request_params_in_query)
+
+        interpol_schema.request_params_in_query["properties"].map do |name, detail|
+          {
+            "in" => "query",
+            "name" => name,
+            "type" => detail["type"],
+          }.merge(enum_params_template(detail))
+            .merge(required_param_template(detail))
+        end
+      end
+
+      def enum_params_template(target)
+        return {} if target["enum"].nil?
+
+        {
+          "enum" => target["enum"],
+        }
+      end
+
+      def required_param_template(target)
+        return {} if target["optional"].nil? || target["optional"] === true
+
+        {
+          "required" => true
+        }
+      end
+
+      def empty_param?(target)
+        target.nil? || target === {}
+      end
+    end
+  end
+end
