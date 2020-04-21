bcabc09149bcccbe1ad72cbf67ad6f1d6734d0b4
ku00 <skentaro36@gmail.com>
2020-04-21 12:28:55 +0900

    thorでコマンドを定義、exe以下に置いて実行できるようにする

--- lib/interpol_schema_2_openapi_schema.rb
+++ lib/interpol_schema_2_openapi_schema.rb
@@ -1,3 +1,5 @@
+require 'thor'
+
 require "interpol_schema_2_openapi_schema/version"
 require "interpol_schema/parser"
 require "openapi_schema/path_object/builder"
@@ -5,9 +7,14 @@ require "openapi_schema/path_object/builder"
 module InterpolSchema2OpenapiSchema
   class Error < StandardError; end
 
-  def self.execute(path)
-    # debug
-    schema = InterpolSchema::Parser.new(path)
-    OpenapiSchema::PathObject::Builder.new(schema)
+  class Cli < Thor
+    default_command :build_path_object
+
+    desc "build_path_object YAML_FILE_PATH", "build OpenAPI path object"
+    def build_path_object(file_path)
+      puts OpenapiSchema::PathObject::Builder.new(
+        InterpolSchema::Parser.new(file_path)
+      ).build
+    end
   end
 end
