8e3b635612bf590a089c68ce2ccf667a31c1c573
ku00 <skentaro36@gmail.com>
2020-04-21 12:57:33 +0900

    [コマンド ファイルパス] でスキーマ生成コマンドが呼び出せるようにする
    ref: https://github.com/erikhuda/thor/issues/370#issuecomment-381423134

--- lib/interpol_schema_2_openapi_schema.rb
+++ lib/interpol_schema_2_openapi_schema.rb
@@ -10,11 +10,14 @@ module InterpolSchema2OpenapiSchema
   class Cli < Thor
     default_command :build_path_object
 
-    desc "build_path_object YAML_FILE_PATH", "build OpenAPI path object"
+    desc "build_path_object YAML_FILE_PATH", "Build OpenAPI path object"
     def build_path_object(file_path)
       puts OpenapiSchema::PathObject::Builder.new(
         InterpolSchema::Parser.new(file_path)
       ).build
     end
+
+    # To make build-command available in no calling itself
+    ARGV.unshift(self.default_command) unless self.all_tasks.has_key?(ARGV[0]) || self.instance_variable_get(:@map).has_key?(ARGV[0])
   end
 end
